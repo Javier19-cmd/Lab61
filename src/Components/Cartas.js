ab74e52fa1f0bcebfd0fcd4c7a3c5620a3a110d4
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-03-26 16:34:24 -0600

    Cambios buenos
    Agregando las transiciones a las cartas y cambiando la parte trasera de las cartas al diseño de yu gi oh.

--- src/Components/Cartas.js
+++ src/Components/Cartas.js
@@ -13,7 +13,7 @@ export default function Cartas({carta, hEleccion, flipped}){
       <div className={flipped ? "flipped" : ""}> {/*Si en caso la carta está volteada, entonces se aplica la clase flipped. Si no lo está, entonces se manda un string vacío*/}
         <img className="frente" src={carta.src} alt="Frente"/> {/*Esta es una imagen dinámica de las cartas*/}
         <img className="trasero"
-        src="/img/uno.png"
+        src="/img/atras.png"
         onClick={handleClick}
         alt="Trasero"
         />
